35094ecfc3a2446a6ce8e841d598acdda13e7dae
tejas-eth <tejaswarambhe@gmail.com>
2023-03-25 16:44:33 +0530

    feat: added Indeed and Internshala API

--- server/index.js
+++ server/index.js
@@ -1,11 +1,63 @@
 const express = require('express');
 const cors = require('cors');
+const internshala = require('./Scrapper/internshala');
+const indeed = require('./Scrapper/indeed');
 const app = express();
 
 const PORT = 4000;
 
 app.use(cors());
 
+app.get('/internshala/:keyword', async(req, res) => {
+
+    const { keyword } = req.params;
+    console.log(keyword);
+    try {
+
+        // Add cron job
+        await internshala(keyword);
+
+        const latestData = require(`./Scrapper/data/internshala-${keyword}`);
+
+        res.status(200).json({
+            status: true,
+            data: latestData
+        })
+
+    } catch (err) {
+        res.status(404).json({
+            status: false,
+            data: err
+        })
+    }
+
+
+})
+
+
+app.get('/indeed/:keyword', async(req, res) => {
+
+    const { keyword } = req.params;
+    console.log(keyword);
+    try {
+
+        await indeed(keyword);
+
+        const indeedLatestData = require(`./Scrapper/data/indeed-${keyword}`);
+
+        res.status(200).json({
+            status: true,
+            data: indeedLatestData
+        })
+
+    } catch (err) {
+        res.status(404).json({
+            status: false,
+            data: err
+        })
+    }
+
+})
 
 
 
