2ac59cfa2b51830c3902c9232dc419901863f35e
tejas-eth <tejaswarambhe@gmail.com>
2023-03-26 05:59:12 +0530

    feat: telegram push notification

--- server/index.js
+++ server/index.js
@@ -7,180 +7,193 @@ const app = express();
 const { linkedin } = require("./Scrapper/linkedin");
 const cron = require("node-cron");
 const shuffle = require("./utils/shuffleArray");
+const sendAlert = require("./utils/getTopInternships");
 const fs = require("fs").promises;
 
 const PORT = 4000;
 
 app.use(cors());
 
-app.get("/internshala/:keyword", async (req, res) => {
-  const { keyword } = req.params;
-  console.log(keyword);
-  try {
-    // Add cron job
-    const hashMap = new Map();
-    hashMap.set("software engineer", "software development");
-    // await internshala(hashMap.get(keyword));
+app.get("/internshala/:keyword", async(req, res) => {
+    const { keyword } = req.params;
+    console.log(keyword);
+    try {
+        // Add cron job
+        const hashMap = new Map();
+        hashMap.set("software engineer", "software development");
+        // await internshala(hashMap.get(keyword));
 
-    const internshalalatestData = require(`./Scrapper/data/internshala-${hashMap.get(
+        const internshalalatestData = require(`./Scrapper/data/internshala-${hashMap.get(
       keyword
     )}`);
 
-    res.status(200).json({
-      status: true,
-      data: internshalalatestData,
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: false,
-      data: err,
-    });
-  }
+        res.status(200).json({
+            status: true,
+            data: internshalalatestData,
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: false,
+            data: err,
+        });
+    }
 });
 
-app.get("/indeed/:keyword", async (req, res) => {
-  const { keyword } = req.params;
-  console.log(keyword);
-  try {
-    // await indeed(keyword);
+app.get("/indeed/:keyword", async(req, res) => {
+    const { keyword } = req.params;
+    console.log(keyword);
+    try {
+        // await indeed(keyword);
 
-    const indeedLatestData = require(`./Scrapper/data/indeed-${keyword}`);
+        const indeedLatestData = require(`./Scrapper/data/indeed-${keyword}`);
 
-    res.status(200).json({
-      status: true,
-      data: indeedLatestData,
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: false,
-      data: err,
-    });
-  }
+        res.status(200).json({
+            status: true,
+            data: indeedLatestData,
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: false,
+            data: err,
+        });
+    }
 });
 
-app.get("/naukri/:keyword", async (req, res) => {
-  const { keyword } = req.params;
-  console.log(keyword);
-  try {
-    // await naukri(keyword);
+app.get("/naukri/:keyword", async(req, res) => {
+    const { keyword } = req.params;
+    console.log(keyword);
+    try {
+        // await naukri(keyword);
 
-    const naukriLatestData = require(`./Scrapper/data/naukri-${keyword}`);
+        const naukriLatestData = require(`./Scrapper/data/naukri-${keyword}`);
 
-    res.status(200).json({
-      status: true,
-      data: naukriLatestData,
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: false,
-      data: err,
-    });
-  }
+        res.status(200).json({
+            status: true,
+            data: naukriLatestData,
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: false,
+            data: err,
+        });
+    }
 });
 
-app.get("/linkedin/:keyword", async (req, res) => {
-  const { keyword } = req.params;
-  console.log(keyword);
-  try {
-    // await linkedin(keyword);
+app.get("/linkedin/:keyword", async(req, res) => {
+    const { keyword } = req.params;
+    console.log(keyword);
+    try {
+        await linkedin(keyword);
 
-    const linkedinLatestData = require(`./Scrapper/data/linkedin-${keyword.replace(
+        const linkedinLatestData = require(`./Scrapper/data/linkedin-${keyword.replace(
       " ",
       "-"
     )}`);
 
-    res.status(200).json({
-      status: true,
-      data: linkedinLatestData,
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: false,
-      data: err,
-    });
-  }
-});
-
-app.get("/browseAll", async (req, res) => {
-  try {
-    const latestBrowseAllData = require("./Scrapper/data/browseAll");
-
-    return res.status(200).json({
-      status: true,
-      data: latestBrowseAllData,
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: false,
-      data: err,
-    });
-  }
-});
-
-app.get("/test", async (req, res) => {
-  try {
-    const keywords = ["software engineer"];
-    let promiseArr = [];
-    const hashMap = new Map();
-    hashMap.set("software engineer", "software development");
-    for (let i = 0; i < keywords.length; i++) {
-      promiseArr.push(linkedin(keywords[i]));
-      promiseArr.push(naukri(keywords[i]));
-      promiseArr.push(internshala(hashMap.get(keywords[i])));
-      promiseArr.push(indeed(keywords[i]));
+        res.status(200).json({
+            status: true,
+            data: linkedinLatestData,
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: false,
+            data: err,
+        });
     }
+});
 
-    const result = await Promise.all(promiseArr);
-    console.log(result, "I was here");
-    let finalResult = result.flat();
+app.get("/browseAll", async(req, res) => {
+    try {
+        const latestBrowseAllData = require("./Scrapper/data/browseAll");
 
-    let randomResult = shuffle(finalResult);
+        return res.status(200).json({
+            status: true,
+            data: latestBrowseAllData,
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: false,
+            data: err,
+        });
+    }
+});
 
-    await fs.writeFile(
-      "Scrapper/data/browseAll.json",
-      JSON.stringify(randomResult, null, 2)
-    );
+app.get("/test", async(req, res) => {
+    try {
+        const keywords = ["software engineer"];
+        // let promiseArr = [];
+        // const hashMap = new Map();
+        // hashMap.set("software engineer", "software development");
+        // for (let i = 0; i < keywords.length; i++) {
+        //     promiseArr.push(linkedin(keywords[i]));
+        //     promiseArr.push(naukri(keywords[i]));
+        //     promiseArr.push(internshala(hashMap.get(keywords[i])));
+        //     promiseArr.push(indeed(keywords[i]));
+        // }
 
-    // const updatedData
+        // const result = await Promise.all(promiseArr);
+        // console.log(result, "I was here");
+        // let finalResult = result.flat();
 
-    return res.status(200).json({
-      status: true,
-      data: randomResult,
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: false,
-      data: err,
-    });
-  }
+        // let randomResult = shuffle(finalResult);
+
+        // await fs.writeFile(
+        //     "Scrapper/data/browseAll.json",
+        //     JSON.stringify(randomResult, null, 2)
+        // );
+
+        await sendAlert("internshala");
+        await sendAlert("naukri");
+        await sendAlert("linkedin");
+        await sendAlert("indeed");
+
+
+        // const updatedData
+
+        return res.status(200).json({
+            status: true,
+            data: [],
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: false,
+            data: err,
+        });
+    }
 });
 
 app.listen(PORT, () => {
-  console.log("Server Connected on PORT: " + PORT);
-  //schedule here
-  // "0 * * * *" every hour
-  cron.schedule("0 * * * *", async () => {
-    const keywords = ["software engineer"];
-    let promiseArr = [];
+    console.log("Server Connected on PORT: " + PORT);
+    //schedule here
+    // "0 * * * *" every hour
+    cron.schedule("0 * * * *", async() => {
+        const keywords = ["software engineer"];
+        let promiseArr = [];
 
-    const hashMap = new Map();
-    hashMap.set("software engineer", "software development");
-    for (let i = 0; i < keywords.length; i++) {
-      promiseArr.push(naukri(keywords[i]));
-      promiseArr.push(internshala(hashMap.get(keywords[i])));
-      promiseArr.push(indeed(keywords[i]));
-      promiseArr.push(linkedin(keywords[i]));
-    }
+        const hashMap = new Map();
+        hashMap.set("software engineer", "software development");
+        for (let i = 0; i < keywords.length; i++) {
+            promiseArr.push(naukri(keywords[i]));
+            promiseArr.push(internshala(hashMap.get(keywords[i])));
+            promiseArr.push(indeed(keywords[i]));
+            promiseArr.push(linkedin(keywords[i]));
+        }
 
-    const result = await Promise.all(promiseArr);
+        const result = await Promise.all(promiseArr);
 
-    let finalResult = result.flat();
+        let finalResult = result.flat();
 
-    let randomResult = shuffle(finalResult);
+        let randomResult = shuffle(finalResult);
 
-    await fs.writeFile(
-      "Scrapper/data/browseAll.json",
-      JSON.stringify(randomResult, null, 2)
-    );
-  });
+        await sendAlert("internshala");
+        await sendAlert("naukri");
+        await sendAlert("linkedin");
+        await sendAlert("indeed");
+
+
+        await fs.writeFile(
+            "Scrapper/data/browseAll.json",
+            JSON.stringify(randomResult, null, 2)
+        );
+    });
 });
